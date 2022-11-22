5adf43dfcae742ffdd6dc4caf8ade4b2219bf884
Dilshan Deshapriya Perera <dilshvn@gmail.com>
2022-11-22 20:36:47 +0530

    Add files via upload

---
+++ codes/Score the Model2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv('https://sololearn.com/uploads/files/titanic.csv')
+df['male'] = df['Sex'] == 'male'
+X = df[['Pclass', 'male', 'Age', 'Siblings/Spouses', 'Parents/Children', 'Fare']].values
+y = df['Survived'].values
+
+model = LogisticRegression()
+model.fit(X, y)
+
+y_pred = model.predict(X)
+print((y == y_pred).sum() / y.shape[0])
