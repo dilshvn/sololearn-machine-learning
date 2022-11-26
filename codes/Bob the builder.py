feda93d0e9219c94babc4d6ff308e54af0d411eb
Dilshan Deshapriya Perera <dilshvn@gmail.com>
2022-11-26 14:30:27 +0530

    Add files via upload

---
+++ codes/Bob the builder.py
@@ -0,0 +1,14 @@
+n = int(input())
+X = []
+for i in range(n):
+    X.append([float(x) for x in input().split()])
+y = [int(x) for x in input().split()]
+datapoint = [float(x) for x in input().split()]
+
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+model = LogisticRegression()
+model.fit(X, y)
+datapoint = np.array(datapoint).reshape(1, -1)
+print(model.predict(datapoint)[0])
